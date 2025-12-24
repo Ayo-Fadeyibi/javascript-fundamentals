f9d73b453160051431de0097b1f3baeb27f6d236
Ayooluwa Fadeyibi <105852858+Ayo-Fadeyibi@users.noreply.github.com>
2025-12-24 23:44:14 +1000

    add simple teacher chatbot using conditionals

---
+++ exercises/03-teacher-chatbot.js
@@ -0,0 +1,11 @@
+// 03-teacher-chatbot.js
+
+let subject = "math";
+
+if (subject === "math") {
+  console.log("Let's practice some numbers!");
+} else if (subject === "english") {
+  console.log("Let's work on your writing skills!");
+} else {
+  console.log("Tell me what subject you'd like help with.");
+}
