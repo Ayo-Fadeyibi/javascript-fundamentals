a9c8dd02a4d436be555d914f1dba6c0c2ccade57
Ayooluwa Fadeyibi <105852858+Ayo-Fadeyibi@users.noreply.github.com>
2026-01-14 17:04:48 +1000

    Applying the filter, map and evenListener methods to filter an array of objects

---
+++ mini-projects/02-Music-Instrument-Filter/script.js
@@ -0,0 +1,31 @@
+const instrumentsArr = [
+  { category: "woodwinds", instrument: "Flute", price: 500 },
+  { category: "woodwinds", instrument: "Clarinet", price: 200 },
+  { category: "woodwinds", instrument: "Oboe", price: 4000 },
+  { category: "brass", instrument: "Trumpet", price: 200 },
+  { category: "brass", instrument: "Trombone", price: 300 },
+  { category: "brass", instrument: "French Horn", price: 4300 },
+  { category: "percussion", instrument: "Drum Set", price: 500 },
+  { category: "percussion", instrument: "Xylophone", price: 3000 },
+  { category: "percussion", instrument: "Cymbals", price: 200 },
+  { category: "percussion", instrument: "Marimba", price: 3000 }
+]
+
+let selectContainer = document.querySelector(".select-container");
+let productsContainer = document.querySelector(".products-container");
+
+function instrumentCards(instrumentCategory) {
+  const instrumentCards = instrumentCategory === "all" ? instrumentsArr : instrumentsArr.filter(instrument => instrument.category === instrumentCategory);
+  return instrumentCards.map(instrument => {
+    return `
+      <div class="card">
+        <h2>${instrument.instrument}</h2>
+        <p>$${instrument.price}</p>
+      </div>
+    `;
+  });
+}
+
+selectContainer.addEventListener("change", () => {
+  productsContainer.innerHTML = instrumentCards(selectContainer.value).join("");
+});
