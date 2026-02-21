d672d41a120005528c7625d07ea35436d27eda09
Ayooluwa Fadeyibi <fadeyibi2016@gmail.com>
2026-02-21 21:40:04 +1000

    Included the routes into the REST structure

--- final-project/LifeTracker/server.js
+++ final-project/LifeTracker/server.js
@@ -5,4 +5,12 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
+const taskRouter = require('./routes/tasks');
+const transactionRouter = require('./routes/transactions');
+const cryptoRouter = require('./routes/crypto');
+
+app.use('/tasks', taskRouter);
+app.use('/transactions', transactionRouter);
+app.use('/crypto', cryptoRouter);
+
 app.listen(port);
